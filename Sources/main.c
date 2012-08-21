11ef1aebbd5c89caf311311e6fa0e00b145e9fb7
Oscar Suro <outercript@gmail.com>
2012-08-20 20:59:06 -0600

    io-compare : blinkear un led con output compare
    io-compare : blinkear un led con output compare
    
    	por fin pudimos usar output compare para
    	blinker un led. no interrupciones solo
    	banderas.
    
    Signed-off-by: Oscar Suro <outercript@gmail.com>

--- Sources/main.c
+++ Sources/main.c
@@ -7,73 +7,67 @@
 const unsigned char flash_security  @0xFF0F = 0xFE;
 
 volatile unsigned int x;
-void TimerDelay(unsigned int time);
-#define  hi_time  1000
-#define  lo_time  2000
+volatile Bool manuel;
 
-/* Codigo para las interrupciones *  
+void TimerInit(void);
+
+/* Start interrupts */  
 #pragma CODE_SEG __NEAR_SEG NON_BANKED
-
-
-interrupt void TimerOverflow_ISR(void){
+__interrupt 0xEE void TimerOverflow_ISR(void){
     PORTA = 0x0F;
-    TIM_TFLG2 = 0x80; // Borra el flag de Interrupcion
+    //TIM_TFLG2 = 0x80; // Borra el flag de Interrupcion
 }
-
-
 #pragma CODE_SEG DEFAULT
-* Terminan las interrupciones *
+/* End interrupts */
 
-void Timer_Init(void){
-    TIM_TSCR2 = 0x87; // TCNT prescaler + Interrupciones Habilitadas
-    TIM_TSCR1 = 0x80; // Timer del Sistema
-    TIM_TFLG2 = 0x80; // Borra el flag de Interrupcion
-}
-*/
+void TimerInit(void){
 
-void TimerDelay (unsigned int time){ // input time in us. 
-    unsigned int ix;
-    unsigned int k; 
-    ix = 0;
-
-    for (ix = 0; ix < 512; ix++){
-        k = 0;
-        for (k = 0; k < 32; k++);
-    }
-        
+    // setup Timer System Control Registers
+    TIM_TSCR1  = 0x80; // TSCR1 - Enable normal timer
+    TIM_TSCR2  = 0x00; // TSCR2 - 0x80 Timer Interrupt Enable
+    TIM_TSCR2 |= 0x07; // TSCR2 - 0x07 128 Prescaler
+    
+    TIM_PACTL  = 0x00; // Setup Timer Preset  
+    //TIM_TFLG2 = 0x80; // Borra el flag de Interrupcion
 }
 
 
 void PeriphInit(void){
     DDRA  = 0x0F; // Configure A[3..0] as outputs 
     PORTA = 0x00; // Output 0
-    
-    // EnableInterrupts;
 }
 
+
 void main(void) {
-    Bool manuel;
     manuel = 0;
         
     PeriphInit();
-    //Timer_Init();
+    TimerInit();
     
+    TIM_TIOS = 0x01;
+    TIM_TIE  = 0x01;
+    TIM_TC0  = TIM_TCNT + 50; 
+    
+    //EnableInterrupts;
     
     for(;;) {
-
+        if(TIM_TFLG1 & TIM_TFLG1_C0F){
+            TIM_TFLG1 = 0x01;
+            PORTA = 0x0F;
+            manuel ^= 1; 
+            TIM_TC0 = TIM_TCNT + 0x0FFF;    
+        }
+        
         if (manuel){   
             PORTA = 0x0F;
-            manuel = 0;
+            //manuel = 0;
         }
         
         else{
             PORTA = 0x00;
-            manuel = 1;
+            //manuel = 1;
         }
         
-        TimerDelay(10);
-      
-           
     } /* loop forever */
-  /* please make sure that you never leave main */
-}
+  
+} /* please make sure that you never leave main */
